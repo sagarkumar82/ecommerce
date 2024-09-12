716a59f25a019c5a7d7e254c0496bcc2ba47afbd
sagarkumar82 <sagarkumar0802001@gmail.com>
2024-09-12 22:33:02 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 // db.js
 const mongoose = require('mongoose');
 
+
 // Connection URI
-const url = 'mongodb://127.0.0.1:27017/nodejs'; // Replace with your MongoDB connection URI and database name
+const url =process.env.MONGO_URI; 
+// const url = 'mongodb://127.0.0.1:27017/nodejs'; // Replace with your MongoDB connection URI and database name
 
 mongoose.connect(url);
 
